6cc721affcc227c08ab086645b187bb4353d6620
m11o <m11o@hey.jp>
2023-08-06 18:30:57 +0900

    implement Display trait for frequency

--- src/word_meaning_searcher/parser/frequency.rs
+++ src/word_meaning_searcher/parser/frequency.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter, Result};
+
 #[derive(Debug, PartialEq)]
 pub enum Frequency {
     Low,
@@ -13,6 +15,17 @@ impl Frequency {
             String::from("high")      => Frequency::High,
             String::from("middle")    => Frequency::Middle,
             String::from("low")       => Frequency::Low
+        }
+    }
+}
+
+impl Display for Frequency {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match *self {
+            Frequency::VeryHigh => write!(f, "very high"),
+            Frequency::High => write!(f, "high"),
+            Frequency::Middle => write!(f, "middle"),
+            Frequency::Low => write!(f, "low")
         }
     }
 }
